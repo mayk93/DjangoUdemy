7c0afab6ab8e42d55d017e600ae30825a5600712
Mihai Mandrescu <mihai.mandrescu@gmail.com>
2015-09-21 17:56:04 +0300

    Generic view update.

--- djangoudemy/polls/urls.py
+++ djangoudemy/polls/urls.py
@@ -1,17 +1,9 @@
 from django.conf.urls import url
 from polls import views
 
-'''
-The urlspatterns variable is a list of url functions.
-They take as arguments:
-1. An url
-2. A view, defined in views.py
-3. A global view name, for namespacing
+#Please see 'pollsUrlsDetails.txt' in the root directory for more details.
 
-We use an empty string (^$) because the last config file removed the 'polls' part, used to get here.
-'''
-
-urlpatterns = [url(r'^$' , views.index, name='index'),
-               url(r'^(?P<questionID>[0-9]+)/$' , views.detail , name='detail'),
-               url(r'^(?P<questionID>[0-9]+)/results$' , views.results , name='results'),
+urlpatterns = [url(r'^$' , views.IndexView.as_view() , name='index'),
+               url(r'^(?P<pk>[0-9]+)/$' , views.DetailView.as_view() , name='detail'),
+               url(r'^(?P<pk>[0-9]+)/results$' , views.ResultsView.as_view() , name='results'),
                url(r'^(?P<questionID>[0-9]+)/vote$' , views.vote , name='vote'),]
--- djangoudemy/polls/views.py
+++ djangoudemy/polls/views.py
@@ -20,30 +20,58 @@ def index(request):
 from django.shortcuts import render, get_object_or_404
 from django.http import Http404, HttpResponseRedirect, HttpResponse
 from django.core.urlresolvers import reverse
+from django.views import generic
 from polls.models import Question, Answer
 
+''' Old 2 Index (Old 2 is different from old) '''
+''' We replaced the url patterns un url.py (polls application urls) with generic views. '''
+''' We now use a class instead of a method '''
+''' The index function is used with the old urls in urls.py '''
+'''
 def index(request):
     latestsQuestionsList = Question.objects.order_by('-publishDate')[:5]
     context = {'latestsQuestionsList':latestsQuestionsList}
     return render(request,'polls/index.html',context) #render return a HTTP response.
+'''
 
+class IndexView(generic.ListView):
+    template_name = 'polls/index.html'
+    context_object_name = 'latestsQuestionsList'
+
+    def get_queryset(self):
+        return Question.objects.order_by('-publishDate')[:5]
+
+''' Old 2 Detail - See Old 2 above index '''
+'''
 def detail(request,questionID):
     #Django generally provides shortcuts for common tasks, like getting an object or raiseing 404 if not found.
     question = get_object_or_404(Question,pk=questionID)
-    ''' try except block no longer needed because question will either be the search object or a 404 error '''
-    '''
+    # try except block no longer needed because question will either be the search object or a 404 error
+    """
     try:
         question = Question.objects.get(pk=questionID) #Pk as in primary key
     except Question.DoesNotExist:
         raise Http404("This question does not exist.")
     else:
-    '''
+    """
     return render(request,'polls/detail.html' , {'question':question})
+'''
 
+class DetailView(generic.DetailView):
+    template_name = 'polls/detail.html'
+    model = Question
+
+''' Old 2 results '''
+'''
 def results(request,questionID):
     question = get_object_or_404(Question,pk=questionID)
     context = {'question':question}
     return render(request,'polls/results.html' , context)
+'''
+
+class ResultsView(generic.DetailView):
+    template_name = 'polls/results.html'
+    model = Question
 
 def vote(request,questionID):
     question = get_object_or_404(Question,pk=questionID)
