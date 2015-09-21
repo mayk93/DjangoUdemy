950ca8001b1281bf3165fa44bbbf5407190b4833
Mihai Mandrescu <mihai.mandrescu@gmail.com>
2015-09-21 14:04:48 +0300

    Basic rendering from view using templates.

--- djangoudemy/polls/views.py
+++ djangoudemy/polls/views.py
@@ -1,10 +1,16 @@
 from django.shortcuts import render
 from django.http import HttpResponse
+from django.template import loader, RequestContext
+from polls.models import Question, Answer
 
 # Create your views here.
 
 def index(request):
-    return HttpResponse('This is the polls application.')
+    latestsQuestionsList = Question.objects.order_by('-publishDate')[:5]
+    template = loader.get_template('polls/index.html')
+    context = RequestContext(request,{'latestsQuestionsList':latestsQuestionsList})
+    #output = " , ".join( [recentQuestion.questionText for recentQuestion in latestsQuestionsList] )
+    return HttpResponse(template.render(context))
 
 def detail(request,questionID):
     return HttpResponse('Question ID: ' + str(questionID))
